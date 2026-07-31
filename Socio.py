8f9997a2a07bdcafd42900b752e0a20a6509ccc4
shakedmoragmarkowsky-commits <shakedmoragmarkowsky@gmail.com>
2026-08-01 03:48:43 +0700

    Update Socio.py

--- Socio.py
+++ Socio.py
@@ -6,6 +6,8 @@ Discord webhook/bot forwarding utilities.
 """
 from flask import Flask, render_template, jsonify, redirect, url_for, session, request
 import base64
+import asyncio
+from twscrape import API, gather
 import json
 import logging
 import os
