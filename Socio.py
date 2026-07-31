718c5d2b8a072706cae65f753c4dae8369a121c1
shakedmoragmarkowsky-commits <shakedmoragmarkowsky@gmail.com>
2026-08-01 03:23:34 +0700

    Update Socio.py

--- Socio.py
+++ Socio.py
@@ -1723,6 +1723,6 @@ def reddit_osint():
 
 
 
-
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
